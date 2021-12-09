870cc86f46e1280f51f3885a99ba8278fdec177c
Cengiz Han <sonsair11@gmail.com>
2021-12-09 10:37:47 +0300

    HomeWork3

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi : " + customer.FirstName);
+        }
+
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Müşteri Güncellendi : " + customer.FirstName);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi : " + customer.FirstName);
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,27 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
+            Customer customer = new Customer();
+            customer.FirstName = "Cengiz Han";
+            customer.LastName = "Uyar";
+            customer.CustomerId = 1;
+            customer.Gender = true;
+
+            Customer customer1 = new Customer();
+            customer1.FirstName = "Zeynep";
+            customer1.LastName = "Uyar";
+            customer1.CustomerId = 2;
+            customer1.Gender = false;
+
+            CustomerManager manager = new CustomerManager();
+            manager.Add(customer);
+            manager.Delete(customer);
+            manager.Update(customer1);
+
             
         }
+        
+      
+
     }
 }
