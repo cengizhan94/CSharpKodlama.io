589af16849a76d45b28ee4eba62472bb8b5b49fe
Cengiz Han <sonsair11@gmail.com>
2022-01-18 14:02:31 +0300

    18-1-2022

--- MyFinalProject/Business/Concrete/ProductManager.cs
+++ MyFinalProject/Business/Concrete/ProductManager.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 
 namespace Business.Concrete
 {
@@ -18,15 +22,14 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
+        [ValidationAspect(typeof (ProductValidator))]
         public IResult Add(Product product)
-        {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
-            _productDal.Add(product);
+        {                                        
+            
+                 _productDal.Add(product);
                 return new SuccessResult(Messages.ProductAdded);
         }
+
         public IDataResult<List<Product>> GetAll()
         {
             if (DateTime.Now.Hour==1)
